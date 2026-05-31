0e925926417bbb3262eaf3e5037a5830c86faa57
Christian Emmanuel Zambrano Hernandez <christianzambher@gmail.com>
2026-05-31 16:17:55 -0600

    feat: implement JWT authentication and protected routes

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Header, Depends, HTTPException
 from auth import crear_token, validar_token
 from fastapi.security import OAuth2PasswordBearer
+from models.login import Login
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="login"
@@ -9,8 +10,14 @@ oauth2_scheme = OAuth2PasswordBearer(
 router = APIRouter()
 
 @router.post("/login")
-def login():
-    token = crear_token({"sub": "usuario"})
+def login(datos: Login):
+    if (datos.username != "admin" or datos.password != "1234"):
+        raise HTTPException(
+            status_code=401,
+            detail="Credenciales inválidas"
+        )
+
+    token = crear_token({"sub": datos.username})
 
     return {"access_token": token}
 
