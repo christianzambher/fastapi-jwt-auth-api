23d2b5731f4570840a18bd8a9ea64d61e8f3a61a
Christian Emmanuel Zambrano Hernandez <christianzambher@gmail.com>
2026-06-03 07:40:11 -0600

    feat: add environment variables configuration

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+if not SECRET_KEY:
+    raise ValueError("La variable de entorno SECRET_KEY no está configurada")
+
+if not ALGORITHM:
+    raise ValueError("La variable de entorno ALGORITHM no está configurada")
